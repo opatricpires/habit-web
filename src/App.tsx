b2908cf7de2be2d361c6c74357e1b57b4bf96ba3
opatricpires <patric.pires@outlook.com>
2023-01-19 09:57:43 -0300

    feat: create initial component

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
 import "./styles/global.css";
 
-import Habit from "./components/Habit";
+import { Header } from "./components/Header";
 
-function App() {
+// import Habit from "./components/Habit";
+
+export function App() {
   return (
-    <>
-      <Habit completed={3} />
-      <Habit completed={10} />
-      <Habit completed={7} />
-    </>
+    <div className="w-screen h-screen flex justify-center items-center">
+      <div className="w-full max-w-5xl px-6 flex flex-col gap-16">
+        <Header />
+      </div>
+    </div>
   );
 }
-
-export default App;

754036d28d4ad253e170021aa99a62172acd4485
opatricpires <patric.pires@outlook.com>
2023-01-26 09:56:26 -0300

    feat: add focus

--- src/components/HabitDay.tsx
+++ src/components/HabitDay.tsx
@@ -29,7 +29,7 @@ export function HabitDay({ defaultCompleted = 0, amount = 0, date }: HabitDay) {
     <Popover.Root>
       <Popover.Trigger
         className={clsx(
-          "w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg",
+          "w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg transition-colors focus:outline-none focus:ring-2 focus:ring-violet-500 focus:ring-offset-2 focus:ring-offset-background",
           {
             "bg-zinc-900 border-zinc-800": completedPercentage === 0,
             "bg-violet-900 border-violet-700":
@@ -46,7 +46,7 @@ export function HabitDay({ defaultCompleted = 0, amount = 0, date }: HabitDay) {
       />
 
       <Popover.Portal>
-        <Popover.Content className="min-w-[320px] p-6 rounded-2xl bg-zinc-900 flex flex-col">
+        <Popover.Content className="min-w-[320px] p-6 rounded-2xl bg-zinc-900 flex flex-col focus:outline-none focus:ring-2 focus:ring-violet-500 focus:ring-offset-2 focus:ring-offset-background">
           <span className="font-semibold text-zinc-400">{dayOfWeek}</span>
           <span className="mt-1 font-extrabold leading-tight text-3xl">
             {dayAndMonth}
--- src/components/NewHabitForm.tsx
+++ src/components/NewHabitForm.tsx
@@ -57,7 +57,7 @@ export function NewHabitForm() {
         id="title"
         placeholder="ex.: Exercícios, dormir bem, etc..."
         autoFocus
-        className="p-4 rounded-lg mt-3 bg-zinc-800 text-white placeholder:text-zinc-400"
+        className="p-4 rounded-lg mt-3 bg-zinc-800 text-white placeholder:text-zinc-400 focus:outline-none focus:ring-2 focus:ring-violet-500 focus:ring-offset-2 focus:ring-offset-zinc-900"
         onChange={(event) => setTitle(event.target.value)}
         value={title}
       />
@@ -70,13 +70,13 @@ export function NewHabitForm() {
         {availableWeekDays.map((weekDay, index) => (
           <Checkbox.Root
             key={weekDay}
-            className="flex items-center gap-3 group"
+            className="flex items-center gap-3 group focus:outline-none   "
             onCheckedChange={() => {
               handleToggleWeekDay(index);
             }}
             checked={weekDays.includes(index)}
           >
-            <div className="h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500">
+            <div className="h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500 transition-colors group-focus:ring-2 group-focus:ring-violet-500 group-focus:ring-offset-2 group-focus:ring-offset-zinc-900">
               <Checkbox.Indicator>
                 <Check size={20} className="text-white" />
               </Checkbox.Indicator>
@@ -89,7 +89,7 @@ export function NewHabitForm() {
 
       <button
         type="submit"
-        className="mt-6 rounded-lg p-4 flex items-center gap-3 font-semibold bg-green-600 justify-center hover:bg-green-500"
+        className="mt-6 rounded-lg p-4 flex items-center gap-3 font-semibold bg-green-600 justify-center hover:bg-green-500 transition-colors focus:outline-none focus:ring-2 focus:ring-green-600 focus:ring-offset-2 focus:ring-offset-zinc-900"
       >
         <Check size={20} weight="bold" />
         Confirmar
